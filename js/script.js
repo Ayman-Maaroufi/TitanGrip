8c392f8bc42917d85f5652ce8108c37b338c5a31
Ayman Maaroufi <ayman.maaroufi@edu.univ-fcomte.fr>
2024-10-26 23:34:04 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,18 +9,3 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    const hamburger = document.querySelector(".hamburger");
-    const navLinks = document.querySelector(".nav-links");
-
-    hamburger.addEventListener("click", () => {
-        hamburger.classList.toggle("active");
-        navLinks.classList.toggle("active");
-    })
-
-    document.querySelectorAll(".nav-links li").forEach(n => n.addEventListener("click", () => {
-        hamburger.classList.remove("active");
-        navLinks.classList.remove("active");
-    }))
-});
